dca6520557a071d99d5ef9f4100d9abab5c97545
Alex Korotkykh <me@alexkorotkikh.com>
2019-12-04 14:07:29 +0100

    day 3 part 1

--- Day3.hs
+++ Day3.hs
@@ -1,19 +1,26 @@
 module Day3 where
 
 import           Data.List.Split
+import           Data.List
+import           Data.Either
+import qualified Data.Set                      as S
+
 
 data Direction = Up Int
                 | Down Int
                 | Leftt Int
                 | Rightt Int
 
-data Point = Point { x :: Int
-                    , y :: Int
-                    } deriving Show
+-- data Point = Point { x :: Int
+--                     , y :: Int
+--                     } deriving Show
 
-data WireSegment = WireSegment { start :: Point
-                               , end :: Point
-                               } deriving Show
+-- data WireSegment = WireSegment { start :: Point
+--                                , end :: Point
+--                                } deriving Show
+
+type Point = (Int, Int)
+type Wire = [Point]
 
 mapDirection :: String -> Direction
 mapDirection ('U' : rest) = Up (read rest :: Int)
@@ -21,57 +28,37 @@ mapDirection ('D' : rest) = Down (read rest :: Int)
 mapDirection ('L' : rest) = Leftt (read rest :: Int)
 mapDirection ('R' : rest) = Rightt (read rest :: Int)
 
-parseWire :: String -> [WireSegment]
+parseWire :: String -> Wire
 parseWire s =
     -- change to foldl and reverse
   let directions = map mapDirection (splitOn "," s)
       segments   = foldl
         (\l d ->
-          let point = if null l then Point 0 0 else end (last l)
-          in  WireSegment
-                  { start = point
-                  , end   = case d of
-                              Up     n -> Point { x = x point, y = y point + n }
-                              Down   n -> Point { x = x point, y = y point - n }
-                              Leftt  n -> Point { x = x point - n, y = y point }
-                              Rightt n -> Point { x = x point + n, y = y point }
-                  }
-                : l
+          let (x, y) = if null l then (0, 0) else last l
+          in  l ++ (case d of
+                Up     n -> [ (x, y') | y' <- [y .. (y + n)] ]
+                Down   n -> reverse $ [ (x, y') | y' <- [(y - n) .. y] ]
+                Rightt n -> [ (x', y) | x' <- [x .. (x + n)] ]
+                Leftt  n -> reverse $ [ (x', y) | x' <- [(x - n) .. x] ]
+              )
+                
         )
         []
         directions
-    -- Wire [WireSegment (Point 0 0) (Point 0 0)]
-  in  reverse segments
+  in  segments
 
 
-findCrosses :: [WireSegment] -> [WireSegment] -> [Point]
-findCrosses as bs = 
-  let options = [ (a, b) | a <- as, b <- bs ]
-      eitherIntersections = map (\ab pq ->
-        case determinant of
-          0 -> Left "(Parallel lines – no intersection)"
-          _ ->
-            let delta f x = f (fst x) - f (snd x)
-                diff [a, b, c, d] = a * d - b * c
-                [abDX, pqDX, abDY, pqDY] = [delta fst, delta snd] <*> [ab, pq]
-                determinant = diff [abDX, abDY, pqDX, pqDY]
-                [abD, pqD] = (\(a, b) -> diff ([fst, snd] <*> [a, b])) <$> [ab, pq]
-                [ix, iy] =
-                  [\(ab, pq) -> diff [abD, ab, pqD, pq] / determinant] <*>
-                  [(abDX, pqDX), (abDY, pqDY)]
-            in Right (ix, iy)
-          ) options
-    in filter isRight eitherIntersections
-  
+findCrosses :: Wire -> Wire -> [Point]
+findCrosses as bs = filter (/= (0, 0))as `intersect` bs
 
 distance :: Point -> Point -> Int
-distance _ _ = 0
+distance (x, y) (x', y') = abs (x - x') + abs (y - y')
 
 main :: IO ()
 main = do
   content <- readFile "day-3-input.txt"
-  let wires = map parseWire (lines content)
-  let crosses      = findCrosses (head wires) (wires !! 1)
+  let wires   = map parseWire (lines content)
+  let crosses = findCrosses (head wires) (wires !! 1)
   print crosses
-  let closestCross = minimum $ map (distance (Point 0 0)) crosses
+  let closestCross = minimum $ map (distance (0, 0)) crosses
   print closestCross
